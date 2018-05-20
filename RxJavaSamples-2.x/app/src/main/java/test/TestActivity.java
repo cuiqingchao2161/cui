65ee59c938676bf3afb342ce9abae36d33ac0b33
qccui <13938442161@163.com>
2018-05-20 18:40:17 +0800

    Signed-off-by: qccui <13938442161@163.com>

--- RxJavaSamples-2.x/app/src/main/java/test/TestActivity.java
+++ RxJavaSamples-2.x/app/src/main/java/test/TestActivity.java
@@ -1,14 +1,20 @@
 package test;
 
 import android.app.Activity;
+import android.app.IntentService;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.rengwuxian.rxjavasamples.R;
+
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.Vector;
 
 import static java.lang.Thread.sleep;
 
@@ -16,7 +22,7 @@ import static java.lang.Thread.sleep;
  * Created by Administrator on 2018/4/29.
  */
 
-public class TestActivity extends Activity{
+public class TestActivity extends Activity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -24,19 +30,33 @@ public class TestActivity extends Activity{
 
         setContentView(R.layout.testlayout);
 //        Button button = findViewById(R.id.button);
+        Log.d("TestActivity","和是："+add(9,34));
 
     }
 
+    public int add(int a, int b) {
+
+        try {
+            Log.d("TestActivity","try 语句块");
+            return a + b;
+
+        } catch (Exception e) {
+
+            Log.d("TestActivity","catch 语句块");
+
+        } finally {
+
+            Log.d("TestActivity","finally 语句块");
+
+        }
+
+        return 0;
+    }
 
 
     public void click(View view) {
+
         Log.w("activityThreadId=", String.valueOf(Thread.currentThread().getId()));
-        startService(new Intent(this,Myservice.class));
-//        try {
-//            sleep(6000);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//        ((Button)findViewById(R.id.button)).setText("结束");
+        startService(new Intent(this, Myservice.class));
     }
 }
